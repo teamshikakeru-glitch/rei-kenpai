87a6de5e567e98974591d45025f803752181d7f9
nakagawakouki <kouki@nakagawakoukinoMacBook-Air.local>
2026-01-14 11:04:40 +0900

    fromアドレスをルートドメインに修正

--- app/api/send-code/route.ts
+++ app/api/send-code/route.ts
@@ -43,7 +43,7 @@ export async function POST(request: NextRequest) {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        from: 'Rei <noreply@send.xn--zcktc494n4ien54d.com>',
+        from: 'Rei <noreply@xn--zcktc494n4ien54d.com>',
         to: email,
         subject: '【Rei】認証コードのお知らせ',
         html: `
